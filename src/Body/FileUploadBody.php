8958d292b2e3f1bb09a89f3f9340e83e20d5aa2a
Brent Scheffler <brent@brentscheffler.com>
2019-09-17 10:41:28 -0700

    Using root namespace for function calls.

--- src/Body/FileUploadBody.php
+++ src/Body/FileUploadBody.php
@@ -7,9 +7,9 @@ use Psr\Http\Message\StreamInterface;
 
 /**
  * @package Shuttle\Body
- * 
+ *
  * Useable only within in a MultpartFormBody.
- * 
+ *
  */
 class FileUploadBody extends BufferBody
 {
@@ -45,16 +45,16 @@ class FileUploadBody extends BufferBody
     {
         if( ($file instanceof StreamInterface) === false ){
             $this->stream = new FileStream(
-                fopen($file, "r")
+                \fopen($file, "r")
             );
 
-            $this->fileName = $fileName ?? basename($file);
-            $this->fileContentType = $contentType ?? mime_content_type($file);
+            $this->fileName = $fileName ?? \basename($file);
+            $this->fileContentType = $contentType ?? \mime_content_type($file);
         }
 
         else {
             $this->stream = $file;
-            $this->fileName = $fileName ?? basename($file->getMetadata('uri') ?? "file");
+            $this->fileName = $fileName ?? \basename($file->getMetadata('uri') ?? "file");
             $this->fileContentType = $contentType ?? 'text/plain';
         }
     }
@@ -68,7 +68,7 @@ class FileUploadBody extends BufferBody
         if( $this->stream->isSeekable() ){
             $this->stream->rewind();
         }
-        
+
         // Build out multi-part
         $multipart = "\r\n--{$boundary}\r\n";
         $multipart .= "Content-Disposition: form-data; name=\"{$name}\"; filename=\"{$this->fileName}\"\r\n";
